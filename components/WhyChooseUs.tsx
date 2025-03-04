21d09c43ad7d548ae4ac07d5bd96f0e10d5899c0
sekorme <asiedua4@uni.coventry.ac.uk>
2025-03-04 16:30:09 +0000

    new ones

--- components/WhyChooseUs.tsx
+++ components/WhyChooseUs.tsx
@@ -60,7 +60,7 @@ const WhyChooseUs = () => {
 
                     className="text-gray-400 text-lg mb-12"
                 >
-                    We make your dreams of working and traveling abroad a reality with affordable, expert consultation.
+                    We make your dreams of working and traveling abroad a reality with affordable, expert consultations.
                 </motion.p>
 
                 {/* Feature Cards */}
