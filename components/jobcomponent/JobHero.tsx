73dfbc9528482dc6c6e6eb09dff32da8ed0adcb9
sekorme <asiedua4@uni.coventry.ac.uk>
2025-04-22 19:02:07 +0000

    background

--- components/jobcomponent/JobHero.tsx
+++ components/jobcomponent/JobHero.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const JobHero = () => {
     return (
-        <div className="w-full  bg-white-gradient grid grid-cols-1 md:grid-cols-2  justify-center items-center">
-            <div className="w-full flex flex-col justify-center mt-10 items-center">
-                <h1 className="text-4xl font-bold text-center">Find Your Dream Job</h1>
+        <div className="w-full  bg-white-gradient dark:bg-dark-gradient grid grid-cols-1 md:grid-cols-2  justify-center items-center">
+            <div className="w-full flex flex-col justify-center mt-10 p-5 items-center">
+                <h1 className="md:text-4xl text-xl font-semibold sm:text-centr capitalize">There are <span className={"text-[#00d346]"}>482,049</span> jobs waiting for you!</h1>
                 <p className="text-lg text-center mt-2">Explore thousands of job opportunities with just one search.</p>
             </div>
             <div className="w-full flex justify-center items-center mt-4">
