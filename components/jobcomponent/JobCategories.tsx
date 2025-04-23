ac83499f18c3aba9c3940cc5245f1c3a6d58ef7a
sekorme <asiedua4@uni.coventry.ac.uk>
2025-04-23 14:59:21 +0000

    orientation

--- components/jobcomponent/JobCategories.tsx
+++ components/jobcomponent/JobCategories.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import {FolderCode} from "lucide-react";
 const JobCategories = () => {
     return (
         <div className={"mt-10"}>
@@ -13,12 +13,22 @@ const JobCategories = () => {
             <div>
                 <div className="grid grid-cols-1 md:grid-cols-3 gap-4 p-5 rounded-xl shadow-xl">
                     {["Software Engineer", "Data Scientist", "Product Manager", "Designer"].map((category, index) => (
-                        <div key={index} className="flex flex-col items-center justify-center  dark:bg-neutral-900 border-1 p-5 hover:shadow-xl  rounded-lg" data-aos={"fade-right"}>
-                            <h2 className="text-lg font-semibold text-gray-500">{category}</h2>
-                            <p className="text-sm text-gray-600 dark:text-gray-400">100+ Jobs</p>
+                        <div
+                            key={index}
+                            className="group flex items-center space-x-10  dark:bg-neutral-900 border-1 p-5 hover:shadow-xl rounded-lg"
+                            data-aos={"fade-right"}
+                        >
+                            <div className="text-gray-400 bg-neutral-200 dark:bg-neutral-800 group-hover:text-white p-5 rounded-xl group-hover:bg-[#049117] dark:text-gray-500 transition-colors duration-300">
+                                <FolderCode size={40}/>
+                            </div>
+                            <div className={"text-start"}>
+                                <h2 className="text-lg font-semibold text-gray-500">{category}</h2>
+                                <p className="text-sm text-gray-600 dark:text-gray-400">100+ Jobs</p>
+                            </div>
                         </div>
                     ))}
                 </div>
+
             </div>
         </div>
     )
